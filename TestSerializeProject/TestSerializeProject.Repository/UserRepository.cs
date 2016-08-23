10d87b84bc368a5a8e691a9d8d6a7960fc4429ce
Эльдар <salyamkin@gmail.com>
2016-08-23 16:45:26 +0600

    Добавил новые классы

---
+++ TestSerializeProject/TestSerializeProject.Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TestSerializeProject.Models;
+
+namespace TestSerializeProject.Repository
+{
+    public class UserRepository
+    {
+        public string GetSerializeUser(User user)
+        {
+            return JsonConvert.SerializeObject(user);
+        }
+    }
+}
--- TestSerializeProject/TestSerializeProject.Test/UserTest.cs
+++ TestSerializeProject/TestSerializeProject.Test/UserTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestSerializeProject.Models;
+using TestSerializeProject.Repository;
 
 namespace TestSerializeProject.Test
 {
@@ -10,11 +12,18 @@ namespace TestSerializeProject.Test
         public void GetSerializeUser()
         {
             //Arrange
+            var userRepository = new UserRepository();
+
+            var user = new User();
+            user.Id = 1;
+            user.Name = "User name";
+
+            var extended = "{\"Id\":1,\"Name\":\"User name\"}";
 
             //Act
-
+            var actual = userRepository.GetSerializeUser(user);
             //Assert
-
+            Assert.AreEqual(extended, actual);
         }
     }
 }
